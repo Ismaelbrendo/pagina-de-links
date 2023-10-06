c40bb37657439652228b99bccd4b88ad76468159
ismaelbrendo <ismael.brendo2@gmail.com>
2023-10-06 17:24:55 -0300

    bug

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function toggleMode() {
     img.setAttribute("src", "./assets/avatar-black.png")
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const html = document.documentElement
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-black.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+})
